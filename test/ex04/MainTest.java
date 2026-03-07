04b54517c910cd5f4eee0423133d30545b5b07fb
Nic0ho <artem555edalov@gmail.com>
2026-03-07 05:14:19 +0200

    add unit tests for ChangeItemCommand and ChangeConsoleCommand

--- test/ex04/MainTest.java
+++ test/ex04/MainTest.java
@@ -1,5 +1,38 @@
 package test.ex04;
 
-public class MainTest {
-    
+import static org.junit.Assert.*;
+import org.junit.Test;
+import ex01.Item2d;
+import ex02.ViewResult;
+import ex04.ChangeItemCommand;
+import ex04.ChangeConsoleCommand;
+
+public class MainTest
+{
+    @Test
+    public void testExecute()
+    {
+        ChangeItemCommand cmd = new ChangeItemCommand();
+        cmd.setItem(new Item2d());
+        double x, y, offset;
+
+        for (int ctr = 0; ctr < 1000; ctr++)
+        {
+            cmd.getItem().setXY(x = Math.random() * 100.0, y = Math.random() * 100.0);
+            cmd.setOffset(offset = Math.random() * 100.0);
+            cmd.execute();
+            assertEquals(x, cmd.getItem().getX(), 1e-10);
+            assertEquals(y * offset, cmd.getItem().getY(), 1e-10);
+        }
+    }
+
+    @Test
+    public void testChangeConsoleCommand()
+    {
+        ChangeConsoleCommand cmd = new ChangeConsoleCommand(new ViewResult());
+        cmd.getView().viewInit();
+        cmd.execute();
+        assertEquals("'c'hange", cmd.toString());
+        assertEquals('c', cmd.getKey());
+    }
 }
